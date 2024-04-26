069a7fcc0739754b838aa1286f889f75274635b6
ShawonECE <send2shawon@gmail.com>
2024-04-26 22:38:02 +0600

    mongodb setup and basic get api setup completed

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5yhhqym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const db = client.db("artifex");
+const coll = db.collection("sculptures");
+
+async function run() {
+    try {
+      await client.connect();
+  
+      app.get('/', async (req, res) => {
+        res.send('Welcome to artifex');
+      });
+  
+      app.get('/sculptures', async (req, res) => {
+          const result = await coll.find({}).toArray();
+          res.send(result);
+      });
+    } 
+    finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.listen(port);
